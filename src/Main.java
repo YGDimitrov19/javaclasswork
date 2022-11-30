e05edf301bfc980e6f7371646a534e1dfc45cdbe
YGDimitrov19 <YGDimitrov19@codingburgas.bg>
2022-11-30 08:37:33 +0200

    classwork

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        Person person1 = new Person("Gosho",5);
+        System.out.println(person1.getName() + " " + person1.getAge());
+    }
+}
